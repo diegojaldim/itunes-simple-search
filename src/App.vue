b048cb97e87ad633ced8ee9c0f198e5773578b04
djaldim <diego@jettax.com.br>
2022-12-05 10:41:38 -0300

    Create album component

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,37 @@
     <div class="flex flex-col h-2/3 w-2/3 border pt-5 pb-5 rounded-sm bg-slate-100 overflow-hidden">
       <div class="flex flex-col pl-5 pr-5 h-full">
 
-        <div class="flex flex-col">
-          <input type="text" v-model="state.term" @keypress.enter="onSearchFromItunes" class="focus:outline-none border border-slate-500 text-sm rounded-sm p-2" placeholder="@TODO...">
-        </div>
+        <form class="flex flex-row" @submit.prevent="onSearchFromItunes">
+          <input
+            type="text"
+            v-model="state.term"
+            class="focus:outline-none flex grow border-t border-l border-b border-slate-500 text-sm rounded-sm rounded-r-none p-2"
+            placeholder="Pesquisar por artista"
+          />
+          <button
+            type="submit"
+            class="bg-green-500 rounded-sm text-slate-50 p-2 font-size pl-10 pr-10"
+          >
+            Buscar
+          </button>
+        </form>
 
+        <div class="flex h-full flex-col overflow-auto">
+          <div
+            v-if="(state.albuns.length === 0 && !state.loading)"
+            class="flex flex-col grow h-full items-center mt-10"
+          >
+            <h3>Nenhum resultado encontrado</h3>
+          </div>
 
-        <div class="flex flex-col grow" style="overflow: auto;">
-          <ul class="border">
-            <li v-for="album in state.albuns">
-              <p>{{ album.collectionName }}</p>
-              <img :src="album.artworkUrl100" :alt="album.collectionName">
-            </li>
-          </ul>
-        </div>
+          <div
+            v-if="state.loading"
+            class="flex flex-col grow h-full items-center mt-10"
+          >
+            <h3>Aguarde...</h3>
+          </div>
 
-        <div class="flex flex-col">
-          <button @click="onSearchFromItunes" class="bg-green-500 rounded-sm text-slate-50 p-2 font-medium">Buscar Artista</button>
+          <Album :albuns="state.albuns" />
         </div>
       </div>
     </div>
@@ -35,33 +50,39 @@ import type {
   SearchFromItunesResponse,
   Artist
 } from './types/itunes.types';
+import Album from './components/Album.vue';
 
 export default {
   setup() {
     interface State {
       term: string;
+      loading: boolean;
       albuns: Array<Artist>;
     };
 
     const state: State = reactive({
-      term: '',
-      albuns: [],
+        term: "",
+        albuns: [],
+        loading: false,
     });
-  
+
     const onSearchFromItunes = async () => {
       const { term } = state;
-
+      state.loading = true;
+      state.albuns = [];
       const response: SearchFromItunesResponse = await searchFromItunes({
-        term,
-        entity: 'album'
+          term,
+          entity: "album"
       });
       state.albuns = response.results;
-    }
+      state.loading = false;
+    };
 
     return {
       state,
       onSearchFromItunes
-    }
-  }
+    };
+  },
+  components: { Album }
 }
 </script>
---
+++ src/components/Album.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col align-middle items-center mt-5 pb-10 border-b-2" v-for="album in props.albuns">
+    <p class="mb-2">Artista: {{ album.artistName }}</p>
+    <p class="mb-2">Álbum: {{ album.collectionName }}</p>
+    <p class="mb-2">Gênero: {{ album.primaryGenreName }}</p>
+    <p class="mb-2">Preço: {{ album.collectionPrice }}</p>
+    <p class="mb-2">Data de Lançamento: {{ album.releaseDate }}</p>
+    <div class="rounded-md overflow-hidden">
+      <a :href="album.artistViewUrl" target="_blank">
+        <img
+          :src="album.artworkUrl100"
+          :alt="album.collectionName"
+        />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type {
+  Artist,
+} from '../types/itunes.types';
+
+const props = defineProps<{
+  albuns: Array<Artist>
+}>();
+</script>
